6bf8c171e02d16257729255401b289bd5f6f584e
Cesar Farrera <cfarrera@nerds.ai>
2018-10-10 10:14:23 -0500

    [0.7.0][Feature] added class 'Person'

---
+++ Database_Management/Person.js
@@ -0,0 +1,46 @@
+"use strict"
+
+class Group{
+
+    constructor(name, realName="", birthDate="", deathDate=""){
+        this._name = name;
+        this._realName = realName;
+        this._birthDate = birthDate;
+        this._deathDate = deathDate;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set realName(realName){
+        this._realName = realName;
+    }
+
+    get realName(){
+        return this._realName;
+    }
+
+    set birthDate(birthDate){
+        this._birthDate = birthDate;
+    }
+
+    get birthDate(){
+        return this._birthDate;
+    }
+
+    set deathDate(deathDate){
+        this.deathDate = deathDate;
+    }
+
+    get deathDate(){
+        return this.deathDate;
+    }
+
+}
+
+module.exports = Group;
