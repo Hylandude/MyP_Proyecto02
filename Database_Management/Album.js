4905b3abe9a7f1f05d3d2ac6c204c68eea9b657c
Cesar Farrera <cfarrera@nerds.ai>
2018-10-10 10:22:45 -0500

    [0.8.0][Feature] Added class 'Album'

---
+++ Database_Management/Album.js
@@ -0,0 +1,37 @@
+"use strict"
+
+class Group{
+
+    constructor(name, path="", year=new Date().getFullYear()){
+        this._name = name;
+        this._path = path;
+        this._year = year;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set path(path){
+        this._path = path;
+    }
+
+    get path(){
+        return this._path;
+    }
+
+    set year(year){
+        this.year = year;
+    }
+
+    get year(){
+        return this.year;
+    }
+
+}
+
+module.exports = Group;
